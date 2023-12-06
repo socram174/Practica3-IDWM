44d39d4c2054af76a873abcfa184a7982e48b5fb
socram174 <socram174@hotmail.com>
2023-12-06 17:51:10 -0300

    añadida dependencia body-parser

--- api/app.js
+++ api/app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 import { User } from './User.js';
 
 const PORT = 3000;
 const MONGO_URL = "mongodb+srv://marcos:marcos@cluster0.7ynbwt5.mongodb.net/Portafolio?retryWrites=true&w=majority";
 const app = express();
+
+app.use(bodyParser.json({extended: true}));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(cors());
 
@@ -14,6 +18,13 @@ app.get('/api/profile',async (req, res) => {
     const user = await User.findOne({email: "marcos.silva@alumnos.ucn.cl"});
 
     res.status(200).send(user);
+});
+
+app.post('/api/profile', async (req, res) => {
+    
+    const user = await User.findById(req.params.id);
+
+    const { type } = req.body;
 });
 
 
